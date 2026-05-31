048d9da68535a4d549d1906dd76275ad7ac7e9a3
Ashish-Saxena1 <ashishsaxena0110@gmail.com>
2026-05-31 12:09:33 +0530

    index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -37,7 +37,7 @@ app.use("/api/messages",messageRoutes)
 if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../Frontend/dist")));
 
-    app.get("(.*)",(req,res)=>{
+    app.get("/:any*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"../Frontend","dist","index.html"));
     });
 
